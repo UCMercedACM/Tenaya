0024bd451e9fc4f475933d6dbec5f042d86ae22c
Adrian Darian <adarian@ucmerced.edu>
2020-02-12 00:13:02 -0800

    switch net/http to gofiber

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module onama
 
 go 1.13
+
+require (
+	github.com/gofiber/fiber v1.4.3
+)
--- main.go
+++ main.go
@@ -1,32 +1,35 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"fmt"
+
+	"github.com/gofiber/fiber"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	switch r.Method {
-	case "GET":
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message": "GET called"}`))
-	case "POST":
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(`{"message": "POST called"}`))
-	case "PUT":
-		w.WriteHeader(http.StatusAccepted)
-		w.Write([]byte(`{"message": "PUT called"}`))
-	case "DELETE":
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message": "DELETE called"}`))
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(`{"message": "not found"}`))
-	}
-}
-
 func main() {
-	http.HandleFunc("/", home)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) {
+		c.Send(`{"message": "Hello World!"}`)
+	})
+
+	// GET /john
+	app.Get("/:name", func(c *fiber.Ctx) {
+		fmt.Printf("Hello %s!", c.Params("name"))
+		// => Hello john!
+	})
+
+	// GET /john/18
+	app.Get("/:name/:age?", func(c *fiber.Ctx) {
+		fmt.Printf("Name: %s, Age: %s", c.Params("name"), c.Params("age"))
+		// => Name: john, Age: 18
+	})
+
+	// GET /api/register
+	app.Get("/api/*", func(c *fiber.Ctx) {
+		fmt.Printf("/api/%s", c.Params("*"))
+		// => /api/register
+	})
+
+	app.Listen(8080)
 }
